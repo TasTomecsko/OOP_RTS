317ef7a701c7f3da028bcf61c343c402ccf33e05
Tomecskó Tas <tomecsko.tas@gmail.com>
2023-11-23 12:26:51 +0100

    Created People class with basic functionallity for naming and giving age.

---
+++ People.cs
@@ -0,0 +1,39 @@
+﻿
+namespace OOP_RTS
+{
+    internal class People
+    {
+        private int age;
+        private string? name;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public string Name 
+        { 
+            get { return name == null ? "Anonymus" : name; } 
+            set { name = value; } 
+        }
+
+        public People() {}
+
+        public People(int age)
+        {
+            this.age = age;
+        }
+
+        public People(string name)
+        {
+            this.name = name;
+        }
+
+        public People(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+    }
+}
